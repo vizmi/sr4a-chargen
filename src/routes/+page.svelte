4795ac222aa8b6a89b072a86d20209860e3598ea
Mihaly Vizhanyo <vizhanyo.mihaly@gmail.com>
2025-03-06 02:23:27 -0800

    fix default-avatar.png

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,13 @@
 </script>
 
 <svelte:head>
-	<script src="https://accounts.google.com/gsi/client" async></script>
+	<script src="https://accounts.google.com/gsi/client" async defer></script>
 </svelte:head>
 <div
 	id="g_id_onload"
 	data-client_id={PUBLIC_GOOGLE_CLIENT_ID}
-	data-context="use"
-	data-auto_select="true"
-	data-close_on_tap_outside="false"
 	data-itp_support="true"
-	data-login_uri="oauth"
+	data-login_uri="http://localhost:5173/oauth"
 ></div>
 <div class="content-container">
 	<h1>Welcome to the Shadows, Chummer!</h1>
